b8178b5d820a15f41546e7f7e3d1883fcfd1adab
Leo Villalobos <villaleobos@gmail.com>
2024-04-27 11:21:59 -0700

    migration: update create-events-table migration

--- db/migrations/000002_create-events-table.up.sql
+++ db/migrations/000002_create-events-table.up.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS organizations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL DEFAULT 'self'
+);
+
 CREATE TABLE IF NOT EXISTS images (
     id INT AUTO_INCREMENT PRIMARY KEY,
     filename VARCHAR(255),
@@ -23,7 +28,7 @@ CREATE TABLE IF NOT EXISTS events (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(1500),
-    organization VARCHAR(100) NOT NULL DEFAULT "self",
+    organization_id INT NOT NULL,
     img_id INT,
     location_id INT,
     price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
@@ -31,8 +36,9 @@ CREATE TABLE IF NOT EXISTS events (
     more_info_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (organization_id) REFERENCES organizations(id),
     FOREIGN KEY (img_id) REFERENCES images(id),
     FOREIGN KEY (location_id) REFERENCES locations(id),
-    FOREIGN KEY (more_info) REFERENCES event_more_info(id),
+    FOREIGN KEY (more_info_id) REFERENCES event_more_info(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
