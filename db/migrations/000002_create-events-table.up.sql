f83c0445591e56855c8890644fd979c7d5cd2e2e
Leo Villalobos <villaleobos@gmail.com>
2024-04-28 10:13:55 -0700

    feat: add events.location_details column

--- db/migrations/000002_create-events-table.up.sql
+++ db/migrations/000002_create-events-table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS organizations (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL DEFAULT 'self'
+    name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS images (
@@ -14,11 +14,11 @@ CREATE TABLE IF NOT EXISTS images (
 
 CREATE TABLE IF NOT EXISTS locations (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL DEFAULT 'TBD',
+    name VARCHAR(255) NOT NULL UNIQUE,
     url VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS event_more_info (
+CREATE TABLE IF NOT EXISTS event_meta (
     id INT AUTO_INCREMENT PRIMARY KEY,
     info VARCHAR(1500),
     url VARCHAR(255)
@@ -28,17 +28,18 @@ CREATE TABLE IF NOT EXISTS events (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(1500),
-    organization_id INT NOT NULL,
+    organization_id INT,
     img_id INT,
     location_id INT,
+    location_details VARCHAR(255),
     price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
     category_id INT NOT NULL,
-    more_info_id INT,
+    meta_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (organization_id) REFERENCES organizations(id),
     FOREIGN KEY (img_id) REFERENCES images(id),
     FOREIGN KEY (location_id) REFERENCES locations(id),
-    FOREIGN KEY (more_info_id) REFERENCES event_more_info(id),
-    FOREIGN KEY (category_id) REFERENCES event_categories(id)
+    FOREIGN KEY (category_id) REFERENCES event_categories(id),
+    FOREIGN KEY (meta_id) REFERENCES event_meta(id)
 );
