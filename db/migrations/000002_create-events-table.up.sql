42ddb88a78565f9b2bd2403122ebcafd5222c12b
Leo Villalobos <villaleobos@gmail.com>
2024-05-11 16:53:57 -0700

    migration: address feedback

--- db/migrations/000002_create-events-table.up.sql
+++ db/migrations/000002_create-events-table.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS images (
     filename VARCHAR(255),
     url VARCHAR(255),
     alt VARCHAR(150),
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
@@ -18,28 +18,22 @@ CREATE TABLE IF NOT EXISTS locations (
     url VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS event_meta (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    info VARCHAR(1500),
-    url VARCHAR(255)
-);
-
 CREATE TABLE IF NOT EXISTS events (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(1500),
     organization_id INT,
-    img_id INT,
+    image_id INT,
     location_id INT,
     location_details VARCHAR(255),
     price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
     category_id INT NOT NULL,
-    meta_id INT,
+    additional_info VARCHAR(1500),
+    additional_url VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (organization_id) REFERENCES organizations(id),
-    FOREIGN KEY (img_id) REFERENCES images(id),
+    FOREIGN KEY (image_id) REFERENCES images(id),
     FOREIGN KEY (location_id) REFERENCES locations(id),
-    FOREIGN KEY (category_id) REFERENCES event_categories(id),
-    FOREIGN KEY (meta_id) REFERENCES event_meta(id)
+    FOREIGN KEY (category_id) REFERENCES event_categories(id)
 );
